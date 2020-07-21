ae9056835226d70a751b13c9854a1d2aca7d0297
jianwei <huang_jianwei1@hotmail.com>
2020-07-21 13:55:37 +0800

    test

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -4,11 +4,11 @@ aws_access_key=$3
 aws_access_secret=$4
 local_path=$5
 
-# Remove any existing versions of a ZIP
-rm -rf $local_path
+# # Remove any existing versions of a ZIP
+# rm -rf $local_path
 
-# Create a zip of the current directory.
-zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
+# # Create a zip of the current directory.
+# zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
 
 # Install required dependencies for Python script.
 pip3 install boto3
